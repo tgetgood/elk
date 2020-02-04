17b22f3df0b058dce35c2b907e408ada5badc466
Thomas Getgood <thomas.getgood@gmail.com>
2020-02-04 11:38:04 -0500

    Very simple editor block interface.

--- src/cljs/elk/components/code.cljs
+++ src/cljs/elk/components/code.cljs
@@ -28,27 +28,72 @@
         [(resolve-binding v) (h/b64-hash body) body]))))
 
 (re-frame/reg-event-fx
+ ::new-block
+ (fn [cofx [_ pre]]
+   (let [s  (->> (:db cofx)
+                 ::codeblocks
+                 vals
+                 (sort-by :index)
+                 (drop-while #(not= (:index %) (:index pre)))
+                 (take 2)
+                 (map :index))
+         i  (if (= 1 (count s))
+              (inc (first s))
+              (/ (reduce + s) 2))
+         id (gensym)]
+     {:db (update (:db cofx) ::codeblocks assoc id {:id id :index i :text ""})
+      ;; HACK: What a kludge
+      :dispatch-later [{:ms 100 :dispatch [::refocus id]}]})))
+
+(re-frame/reg-event-fx
+ ::refocus
+ (fn [_ id]
+   {::focus id}))
+
+(re-frame/reg-fx
+ ::focus
+ (fn [[_ id]]
+   (println id (.getElementById js/document id))
+   (-> (.getElementById js/document id)
+    .-childNodes
+    (aget 0)
+    .-CodeMirror
+    .focus)))
+
+(re-frame/reg-event-fx
  ::unfocus
  (fn [{:keys [db]} [_ id]]
    (try
      (let [text (get-in db [::codeblocks id])
-           [v h body] (interpret text)]
-       (println (ev (cljs.reader/read-string text) )))
+           [v h body] (interpret text)])
      (catch js/Error e
        (do
          (println e)
          {:dispatch [::read-error id]})))))
 
 (re-frame/reg-sub
- ::codeblock
+ ::content
  (fn [db [_ id]]
-   (or (get-in db [::codeblocks id]) "")))
+   (get-in db [::codeblocks id :text])))
 
-(defn codearea [{:keys [id]}]
+(re-frame/reg-sub
+ ::codeblocks
+ (fn [db]
+   (::codeblocks db)))
+
+(defn codearea [{:keys [id] :as opts}]
   (let [editor  (atom nil)
-        content @(re-frame/subscribe [::codeblock id])]
+        content @(re-frame/subscribe [::content id])]
     (reagent/create-class
-     {:reagent-render      (fn [] [:div])
+     {:reagent-render      (fn [] [:div
+                                   {:id    id
+                                    :style {:border        "0.1rem"
+                                            :height        :max-content
+                                            :margin-top    "1rem"
+                                            :padding       "0.5rem"
+                                            :border-style  "outset"
+                                            :border-color  "rgba(100,100,100,0.1)"
+                                            :border-radius "0.5rem"}}])
       :component-did-mount (fn [this]
                              (reset! editor
                                      (js/CodeMirror (reagent/dom-node this)
@@ -56,6 +101,13 @@
                                                      {:mode        :clojure
                                                       :lineNumbers false
                                                       :value       content})))
+                             (.on @editor "keypress"
+                                  (fn [ed ev]
+                                    (when (and (.-shiftKey ev)
+                                               (= (.-key ev) "Enter"))
+                                      (.preventDefault ev)
+                                      (.stopPropagation ev)
+                                      (re-frame/dispatch [::new-block opts]))))
                              (.on @editor "change"
                                   (fn [ed change]
                                     (re-frame/dispatch
@@ -66,7 +118,11 @@
                                      [::unfocus id]))))})))
 
 (defn unit [{:keys [id] :as opts}]
-  [:div.column
+  [:div.column {:key id
+                :style {:width :max-content
+                        :max-width "39rem"
+                        :height :max-content
+                        :margin-bottom "1rem"}}
    [:div.row
     (let [id (gensym)]
       [:div
@@ -78,3 +134,8 @@
        [:label {:for id} "metadata"]
        [:input {:id id :type :text}]])]
    [codearea opts]])
+
+(defn editor-page []
+  (let [blocks @(re-frame/subscribe [::codeblocks])]
+    [:div.column
+     (map unit (sort-by :index (vals blocks)))]))
--- src/cljs/elk/db.cljs
+++ src/cljs/elk/db.cljs
@@ -1,4 +1,7 @@
 (ns elk.db)
 
 (def default-db
-  {})
+  {:elk.components.code/codeblocks (let [id (gensym)]
+                                     {id {:id id
+                                          :index 0
+                                          :text ""}})})
